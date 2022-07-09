b593606b6417922bb6cee75df7f180e6377ba655
securecodeninja <9094428+securecodeninja@users.noreply.github.com>
2022-07-08 18:24:08 -0700

    Update stacktrace-disclosure.cs

--- stacktrace-disclosure.cs
+++ stacktrace-disclosure.cs
@@ -1,7 +1,22 @@
 public void Configure(IApplicationBuilder app, IWebHostEnvironment env)  
 {  
-        if (!env.IsDevelopment())  
+        if (!env.IsDevelopment())
         {  
+            // ruleid: stacktrace-disclosure
+            app.UseDeveloperExceptionPage();  
+        }  
+    else  
+        {  
+            app.UseExceptionHandler("/Error");  
+        }  
+
+}
+
+public void Configure(IApplicationBuilder app, IWebHostEnvironment env)  
+{  
+        if (env.IsDevelopment())  
+        {  
+            // ok: stacktrace-disclosure                
             app.UseDeveloperExceptionPage();  
         }  
     else  
